b53566827183574845aa7059853cd8f2a94dcbbc
Daniil <daniil.toropov@mail.ru>
2023-11-19 22:55:05 +0300

    Final

--- CharacterAPI/View/CharacterViewCell.swift
+++ CharacterAPI/View/CharacterViewCell.swift
@@ -14,14 +14,12 @@ final class CharacterViewCell: UITableViewCell {
     @IBOutlet weak var labelLocation: UILabel!
     @IBOutlet weak var labelSpecies: UILabel!
     @IBOutlet weak var labelName: UILabel!
-    
-    
+
     func configur(character: Character) {
         labelName.text = character.name
         labelSpecies.text = character.species
         labelLocation.text = character.location.name
         //        labelEpisode.text = character.episode
-        
         
         NetworkManager.shared.fetchImageCharacters(url: character.image) { result in
             switch result {
